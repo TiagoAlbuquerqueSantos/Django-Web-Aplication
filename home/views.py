eba72405f47d9edda1abc95bed68bae41711fd3b
TiagoAlbuquerqueSantos <tiagoalbuquerque090@gmail.com>
2025-08-24 19:11:04 -0300

    Banco de Dados
    Integração com o Banco de Dados

--- home/views.py
+++ home/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Usuario
 
 
 def pagina_inicial(request):
-    return render(request, 'home.html', {'nome': 'Tiago'})
+    if request.method == 'GET':
+        return render(request, 'home.html', {'nome': 'Tiago'})
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        idade = request.POST.get('idade')
+
+        user = Usuario.objects.filter(nome=nome)
+
+      #  user = Usuario(nome=nome, idade=idade)
+      #  user.save()
+
+        return HttpResponse(f'O usuário {nome} foi cadastrado com sucesso!')
 
 
 def dashboards(request):
-    return HttpResponse('Estatísticas do Site')
+
+    pessoas = Usuario.objects.all()
+    return HttpResponse(pessoas)
